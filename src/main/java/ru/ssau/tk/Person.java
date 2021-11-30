f1314cde85fe3f310a952a412799771c48d42a81
hovanoff <hovidze93@mail.com>
2021-12-01 03:40:19 +0400

    первое задание

---
+++ src/main/java/ru/ssau/tk/Person.java
@@ -0,0 +1,37 @@
+package ru.ssau.tk;
+
+public class Person {
+    private String firstName;
+    private String secondName;
+    private int personId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getSecondName() {
+        return secondName;
+    }
+    public int getPersonId() {
+        return personId;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void  setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public static void main(String[] args) {
+        Person firstPerson = new Person();
+        Person secondPerson = new Person();
+        firstPerson.setFirstName("Josh");
+        firstPerson.setSecondName("Wilson");
+        firstPerson.setPersonId(123321);
+        secondPerson.setFirstName("John");
+        secondPerson.setSecondName("Wilsones");
+        secondPerson.setPersonId(1233213);
+    }
+}
