6f0552ed1d2abdf49bed117cddc10a06a957c4af
hovanoff <hovidze93@mail.com>
2021-12-29 12:19:18 +0400

    1.4

--- src/main/java/TypeOfData/Point.java
+++ src/main/java/TypeOfData/Point.java
@@ -11,6 +11,18 @@ public class Point {
         this.z = z;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
 
 
 }
